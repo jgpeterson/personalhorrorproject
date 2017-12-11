7ebe9b595487ce1baaa4a791040f39e8717a3bd0
jamespeterson <jamespeterson0918@yahoo.com>
2017-12-11 13:40:59 -0500

    setting up main menu

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -11,7 +11,12 @@ class MainMenu extends Component {
     render() {
         return (
             <div>
-                <Header />
+            <Header />
+             <h1>Shout Factory announces Pre-Orders for The Strangers Blu-Ray</h1>
+             <img src="https://i.imgur.com/Dqa19hh.png"/>
+             < br/>
+             <a href="https://www.shoutfactory.com/product/the-strangers-collector-s-edition?product_id=6621">Click to Pre-Order</a>
+                
             </div>
         );
     }
