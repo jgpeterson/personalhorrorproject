7c16ac506aa66553568970dedacbe92bc135d85a
jamespeterson <jamespeterson0918@yahoo.com>
2017-12-11 13:27:19 -0500

    stylized the podcast a bit

--- client/src/components/Podcasts.js
+++ client/src/components/Podcasts.js
@@ -4,6 +4,31 @@ import styled from 'styled-components'
 import Header from './Header'
 import axios from 'axios'
 
+const FlexContainer = styled.div`
+text-align: center;
+display: inline-block;
+width: 85%;
+background: rgba(0, 0, 0, 0.5);
+border: 1px solid;
+margin-bottom: 8px;
+margin-right: 8px;
+margin-left: 170px;
+border-radius: 50px;
+padding: 15em;
+position: relative;
+min-width: 340px;
+max-width: 850px;
+@media screen and (max-width:450px) {
+ .logo img {
+     font-size: 1.25rem;
+     max-width: 700px;
+ }
+ @media screen and (max-width:450px) {
+   background: none;
+ }
+`
+
+
 class Podcasts extends Component {
     state = {
         podcasts: [],
@@ -23,14 +48,15 @@ class Podcasts extends Component {
 
         const videos = this.state.podcasts.map((video) => {
             return (
-                
+                <FlexContainer>
                 <div>
+                <h1>Podcast List</h1>
                     {video.title}
                     <div>
                     <iframe src={video.description}/>
                     </div>
                 </div>
-               
+               </FlexContainer>
             )
         })
 
